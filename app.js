fb0c849cdc29a1e9de582d0760544be13d7993bd
firuaun <firuaun@gmail.com>
2015-04-28 11:35:09 +0200

    removing message from queue just after it is received

--- app.js
+++ app.js
@@ -21,44 +21,44 @@ var s3 = new AWS.S3();
                    receivedMsg.Messages[0].MessageAttributes.hasOwnProperty('Key')){
                    var attr = receivedMsg.Messages[0].MessageAttributes;
                     console.log("Wiaderko:",attr.Bucket.StringValue,"Plik:",attr.Key.StringValue);
-                    var objParams = {Bucket:attr.Bucket.StringValue,
-                                    Key:attr.Key.StringValue};
-                    s3.getObject(objParams,function(err, data){
-                            if(err) {
-                                console.log("Can't get object",err);
-                                return;
-                            }
-                            SqsCmd.send(SQS,queueUrl,"Obliczone :D",function(err,data){
-                                if(err){
-                                    console.log("Can't put msg on queue",err);
-                                    return;
-                                }
-                                SqsCmd.delete(SQS,queueUrl,receivedMsg.Messages[0].ReceiptHandle,function(err,data){
+                    SqsCmd.delete(SQS,queueUrl,receivedMsg.Messages[0].ReceiptHandle,function(err,data){
                                     if(err){
                                         console.log("Can't remove message",err);
                                         return;
                                     }
-                                    console.log("DONE");   
+                                    var objParams = {Bucket:attr.Bucket.StringValue,
+                                                    Key:attr.Key.StringValue};
+                                    s3.getObject(objParams,function(err, data){
+                                            if(err) {
+                                                console.log("Can't get object",err);
+                                                return;
+                                            }
+                                            SqsCmd.send(SQS,queueUrl,"Obliczone :D",function(err,data){
+                                                if(err){
+                                                    console.log("Can't put msg on queue",err);
+                                                    return;
+                                                }
+                                                console.log("DONE"); 
+                                            },{
+                                                Filename:{
+                                                    DataType:'String',
+                                                    StringValue: objParams.Key
+                                                },
+                                                Bucket:{
+                                                    DataType:'String',
+                                                    StringValue: objParams.Bucket
+                                                },
+                                                Hash:{
+                                                    DataType:'String',
+                                                    StringValue: helpers.calculateDigest("md5",data.Body,'hex')
+                                                }
+                                            });
+                                        });
                                 });
-                            },{
-                                Filename:{
-                                    DataType:'String',
-                                    StringValue: objParams.Key
-                                },
-                                Bucket:{
-                                    DataType:'String',
-                                    StringValue: objParams.Bucket
-                                },
-                                Hash:{
-                                    DataType:'String',
-                                    StringValue: helpers.calculateDigest("md5",data.Body,'hex')
-                                }
-                            });
-                        });
-                }
-                //TODO: do shit and fuck bitches, like a boss
+                    }
                 return _pool();
             }
+            
         },['Bucket','Key']);
     }
    // while(true){
