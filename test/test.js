f4573441632aca447401885728436381f1aa7221
firuaun <firuaun@gmail.com>
2015-06-26 15:54:10 +0200

    added test and config

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+
+describe("digestServer project test",function(){
+    it("should pass",function(){
+        assert.equal(true, true, "impossible is now possible");
+    });
+});
